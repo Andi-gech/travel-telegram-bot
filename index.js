e1539ef859e32012fafb01c75c70bed37b55ee60
andi-gech <andi.fab23@gmail.com>
2025-04-06 14:48:42 -0700

    dd

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const connectDB = require('./db')
 
 // === Configuration ===
 const BOT_TOKEN = process.env.BOT_TOKEN
-console.log('BOT_TOKEN:', BOT_TOKEN);
+console.error('BOT_TOKEN:', BOT_TOKEN);
 
 const ADMIN_ID = [445168632]; // Replace with actual admin IDs
 
