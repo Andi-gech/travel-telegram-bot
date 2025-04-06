4f74a6b213d3654a2f6bbd667c5ae838e45f583f
andi-gech <andi.fab23@gmail.com>
2025-04-06 14:52:19 -0700

    chang

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase';
 console.error('Mongo URI:', MONGO_URI); // Log the Mongo URI for debugging
+console.error('BOT_TOKEN:', process.env.BOT_TOKEN);
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI);
